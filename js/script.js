67faa4edb0898f2b651666e3fbf08396799d4fe6
lekdeche <ali.lekdech@gmail.com>
2021-06-01 20:35:47 +0200

    ajout evenement sur Bouton Supprimer

--- js/script.js
+++ js/script.js
@@ -5,14 +5,31 @@ const plusButtons = document.querySelectorAll('#first-playlist button');
 const pListInput = document.getElementById('list-group');
 const pListSpan = document.getElementById('ResultPlayList');
 
+const removeButton = document.querySelector('#playlist button');
+const removeList = document.getElementById('ResultPlayList');
+
 
 plusButtons.forEach(button => {
   button.addEventListener('click', () => {
     console.log(button.dataset.song);
     const newPlayList1 = document.createElement('li');
     const text = document.createTextNode(button.dataset.song);
-    nouvelLi.append(text);
-    pListSpan.append(nouvelLi);
+
+    const del = document.createElement('button');
+    newPlayList1.append(del);
+    del.textContent = "Delete";
+
+    del.addEventListener('click', () => {
+      newPlayList1.remove(text);
+
+    })
+
+    newPlayList1.append(text);
+    pListSpan.append(newPlayList1);
+
   })
+  
 })
+
+
 
