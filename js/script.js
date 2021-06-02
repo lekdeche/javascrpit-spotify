ea4d57c6684a5c1aba9b4302f9ebc78715106134
lekdeche <ali.lekdech@gmail.com>
2021-06-02 11:20:06 +0200

    Ajout acces spotify

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ const pListSpan = document.getElementById('ResultPlayList');
 const removeButton = document.querySelector('#playlist button');
 const removeList = document.getElementById('ResultPlayList');
 
+const spotifyAcces = document.getElementById('spotify');
+
+
+
 
 plusButtons.forEach(button => {
   button.addEventListener('click', () => {
@@ -16,20 +20,45 @@ plusButtons.forEach(button => {
     const text = document.createTextNode(button.dataset.song);
 
     const del = document.createElement('button');
-    newPlayList1.append(del);
     del.textContent = "Delete";
-
     del.addEventListener('click', () => {
       newPlayList1.remove(text);
-
+      
     })
-
+    
     newPlayList1.append(text);
+    newPlayList1.append(del);
     pListSpan.append(newPlayList1);
-
+    
   })
-  
+
+});
+
+// 1) Connexion Api Spotify (access token)
+
+
+
+spotifyAcces.addEventListener('click',() => {
+
+
+  const spotify = "https://accounts.spotify.com/authorize?response_type=code&client_id=a12a62138a824884805c61fde334ed10&redirect_uri=http://127.0.0.1:5500/javascript-spotify/";
+
+  document.location.href=spotify
+
+
 })
+
+
+
+// 2) Request Api Spotify
+
+
+
+
+
+
+
+
 
 
 
